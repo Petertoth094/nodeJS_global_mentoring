8feb04c1b5a11eee95aa779a4f9e0eaf5a55d176
Peter Toth9 <peter_toth9@epam.com>
2022-01-17 08:05:40 +0100

    task1 done

---
+++ HW1/task1.js
@@ -0,0 +1,21 @@
+/**
+ * Write a program which reads a string from the standard input stdin, reverses it and then writes it to the standard output stdout.
+ * 
+ * The program should be started from npmscript via nodemon(i.e. npm run task1).
+ * The program should be running in a stand-by mode and should not be terminated after the first-stringprocessing.
+ */
+
+const readline = require('readline')
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+r1.on('line', function(line){
+    console.log(line.split('').reverse().join('') + '\n');
+})
+
+
+// process.stdin.on('data', data => {
+//     console.log(`${data.toString().split('').reverse().join('') + '\n'}`);
+//   });
